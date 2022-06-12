617201d1a6d15b59e398f08b85e1c6e832d65ecf
juanxiushigan2017 <szphhyxl@126.com>
2022-06-13 01:14:47 +0800

    change condition

--- main.py
+++ main.py
@@ -1,28 +1,24 @@
 import discord
 import os
 from web3 import Web3
-import requests
 import codecs
+import requests
 
 client = discord.Client()
 web3 = Web3(Web3.HTTPProvider('https://rpc.ankr.com/eth'))
-baseUrl = 'https://raw.githubusercontent.com/ethereum-lists/4bytes/master/signatures/'
+dictMap = {}
+etherScanUrl = 'https://api.etherscan.io/api?module=contract&action=getsourcecode&apikey=' + os.getenv('ESAPIKEY')
 
-blockNumber = web3.eth.get_block_number()
-print(blockNumber)
-transCount = web3.eth.get_block_transaction_count(blockNumber)
-for index in range(transCount):
-    transaction = web3.eth.get_transaction_by_block(blockNumber, index)
-    if (transaction.to != None and transaction.input != '0x'):
-        # contract = web3.eth.contract(transaction.to)
-        # result = requests.get(baseUrl + transaction.input[2:10])
-        try:
-            f = codecs.open('./signatures/' + transaction.input[2:10], 'r', 'utf-8')
-        except IOError:
-        	f.close()
-        else:
-        	print(f.read())
-        	f.close()
+try:
+    f = codecs.open('./signature', 'r', 'utf-8')
+except IOError:
+	f.close()
+else:
+	for line in f:
+		keyValue = line.split('=')
+		dictMap[keyValue[0]] = keyValue[1]
+	
+	f.close()
 
 @client.event
 async def on_connect():
@@ -30,6 +26,16 @@ async def on_connect():
 		global messageChannel
 		if (channel.name == 'free_mint'):
 			messageChannel = channel
+
+	blockNumber = web3.eth.get_block_number()
+	transactions = web3.eth.get_block(blockNumber, True).transactions
+	for transaction in transactions:
+	    if (transaction.to != None and transaction.input != '0x'):
+	    	key = transaction.input[2:10]
+	    	if (key in dictMap):
+	            if ('mint' in dictMap[key] and transaction.value == 0):#增加logs判定
+	            	# messageChannel.send('free mint=' + transaction.to)
+	            	requests.get('')
 
 @client.event
 async def on_ready():
@@ -46,7 +52,6 @@ async def on_message(message):
 token = os.getenv('TOKEN')
 
 if (token is not None):
-	# client.run(token)
-	print(111)
+	client.run(token)
 else:
     print('Token is None')
